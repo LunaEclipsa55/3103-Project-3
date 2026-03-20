4a5d923ba2a268d83aee66067f93dbde45719336
LunaEclipsa <Brantuo16@outlook.com>
2026-03-20 07:23:08 -0400

    eh

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    [SerializeField] float MoveSpeed = 5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
